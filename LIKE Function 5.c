430d5105336e186a0bf597339bc4842b4e0eba2c
Md. Daudul Islam Opu <140642063+Dawadulopu88@users.noreply.github.com>
2023-08-15 12:12:50 +0600

    Add files via upload

---
+++ LIKE Function 5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+LIKE(int x){
+    int i;
+    for(i=1;i<=x;i++)
+        {
+printf("Like ");
+        }
+}
+int main (){
+    int x;
+    scanf("%d",&x);
+    printf("I ");
+    LIKE(x);
+    printf("You");
+
+}
+
+

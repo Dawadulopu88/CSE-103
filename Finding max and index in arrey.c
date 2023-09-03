9d9e1e678049a88ec8ae0b1b2fb9bf74a73c6e14
Md. Daudul Islam Opu <140642063+Dawadulopu88@users.noreply.github.com>
2023-09-03 20:14:07 +0600

    Add files via upload

---
+++ Finding max and index in arrey.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("How many numbers");
+    scanf("%d",&n);
+    int a[n],i,max,idx;
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    idx=0;
+    max=a[0];
+    for(i=1; i<n; i++)
+    {
+        if(max<a[i])
+            max=a[i];
+        idx=i;
+    }
+    printf("Max %d & Index %d",max,idx );
+}

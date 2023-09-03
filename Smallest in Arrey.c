636fc2b0071acfd3e125bc5331f9e2eb0d86638a
Md. Daudul Islam Opu <140642063+Dawadulopu88@users.noreply.github.com>
2023-09-03 20:14:39 +0600

    Add files via upload

---
+++ Smallest in Arrey.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int smallest = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+        }
+    }
+
+    printf("Smallest = %d\n", smallest);
+
+    return 0;
+}
+
